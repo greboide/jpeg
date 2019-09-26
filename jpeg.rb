32c9ddee008a1fd1d57e97d59696d6a34ea1eeb3
Glauber Prado <prado.glauber@unifesp.br>
2019-09-26 13:42:26 -0300

    working encoding decoding

--- jpeg.rb
+++ jpeg.rb
@@ -9,7 +9,9 @@ class MyJPEG
   def initialize(file)
     @file_data = File.read(file).split
     @mpixels = @file_data.dup
-    @dctpixels = []
+    @dctpixels_quantized = []
+    @dctpixels_quantized_sorted = []
+    @idctpixels = []
     @file_size = @file_data[10]
     @first_pixel = 12
     @T = Matrix[[0.3536, 0.3536, 0.3536, 0.3536, 0.3536, 0.3536, 0.3536, 0.3536],
@@ -30,11 +32,11 @@ class MyJPEG
                   [72,92,95,98,112,100,103,99]]
   end
 
-  def chunks
-    counter = 0
+  def encode
     (12..409_612).each_slice(5120) do |i|
+      counter = 0
       box = Matrix[]
-      i.each do |a|
+      (i.first..(i.first+640)).each do |a|
         column = [@mpixels[a].to_i - 128,
                   @mpixels[a+640].to_i - 128,
                   @mpixels[a+1280].to_i - 128,
@@ -51,8 +53,8 @@ class MyJPEG
               end
         if counter == 7
           dct = @T*box*@T.transpose
-          quantized  = [[],[],[],[],[],[],[],[]] 
-          quantized_sorted  = [[],[],[],[],[],[],[],[]] 
+          quantized  = [[],[],[],[],[],[],[],[]]
+          quantized_sorted  = [[],[],[],[],[],[],[],[]]
           dct.each_with_index {|b,i,j| quantized[i][j] = (b/@Q50[i,j]).round}
           row_sorted= 0
           column_sorted=0
@@ -69,9 +71,6 @@ class MyJPEG
               else
                 column_sorted += 1
               end
-              # if idx == 7 && column.zero?
-              #   row += 1
-              # end
               break if column == idx_row
               row += 1
               column -= 1
@@ -87,19 +86,28 @@ class MyJPEG
             end
             column = idx
           end
-          binding.pry
 
-          idx = 7
-          for i in ((a-8)..a)
-            @dctpixels[i] = quantized_sorted[idx][0]
-            @dctpixels[i + 640] = quantized_sorted[idx][1]
-            @dctpixels[i + 1280] = quantized_sorted[idx][2]
-            @dctpixels[i + 1920] = quantized_sorted[idx][3]
-            @dctpixels[i + 2560] = quantized_sorted[idx][4]
-            @dctpixels[i + 3200] = quantized_sorted[idx][5]
-            @dctpixels[i + 3840] = quantized_sorted[idx][6]
-            @dctpixels[i + 4480] = quantized_sorted[idx][7]
-            idx -= idx
+          idx = 0
+          for i in ((a-7)..a)
+            @dctpixels_quantized_sorted[i] = quantized_sorted[idx][0]
+            @dctpixels_quantized_sorted[i + 640] = quantized_sorted[idx][1]
+            @dctpixels_quantized_sorted[i + 1280] = quantized_sorted[idx][2]
+            @dctpixels_quantized_sorted[i + 1920] = quantized_sorted[idx][3]
+            @dctpixels_quantized_sorted[i + 2560] = quantized_sorted[idx][4]
+            @dctpixels_quantized_sorted[i + 3200] = quantized_sorted[idx][5]
+            @dctpixels_quantized_sorted[i + 3840] = quantized_sorted[idx][6]
+            @dctpixels_quantized_sorted[i + 4480] = quantized_sorted[idx][7]
+            @dctpixels_quantized[i] = quantized[idx][0]
+            @dctpixels_quantized[i + 640] = quantized[idx][1]
+            @dctpixels_quantized[i + 1280] = quantized[idx][2]
+            @dctpixels_quantized[i + 1920] = quantized[idx][3]
+            @dctpixels_quantized[i + 2560] = quantized[idx][4]
+            @dctpixels_quantized[i + 3200] = quantized[idx][5]
+            @dctpixels_quantized[i + 3840] = quantized[idx][6]
+            @dctpixels_quantized[i + 4480] = quantized[idx][7]
+            idx += 1
+          end
+          if @dctpixels_quantized[19] != 0
           end
           box = Matrix[]
         end
@@ -111,12 +119,69 @@ class MyJPEG
       end
     end
   end
-  def write_pgm
-    f = File.new('steph2.pgm', 'w')
-    f.write("P2\n")
-    f.write("640 640 \n")
-    f.write("256\n")
-    f.close
+  def decode
+    counter = 0
+    (12..409_612).each_slice(5120) do |i|
+      box = Matrix[]
+      i.each do |a|
+        column = [@dctpixels_quantized[a].to_i,
+                  @dctpixels_quantized[a+640].to_i,
+                  @dctpixels_quantized[a+1280].to_i,
+                  @dctpixels_quantized[a+1920].to_i,
+                  @dctpixels_quantized[a+2560].to_i,
+                  @dctpixels_quantized[a+3200].to_i,
+                  @dctpixels_quantized[a+3840].to_i,
+                  @dctpixels_quantized[a+4480].to_i]
+        mcolumn = Matrix[column]
+        box = if counter.zero?
+                Matrix.vstack(mcolumn)
+              else
+                box.vstack(mcolumn)
+              end
+        if counter == 7
+          reconstructed  = [[],[],[],[],[],[],[],[]]
+          n  = [[],[],[],[],[],[],[],[]]
+          box.each_with_index {|b,i,j| reconstructed[i][j] = b*@Q50[i,j]}
+          rec = Matrix[reconstructed[0]]
+          reconstructed.each_with_index { |b,i| rec = rec.vstack(Matrix[b]) if i != 0 }
+          idct = @T.transpose*rec*@T
+          idct.each_with_index{ |b,i,j| n[i][j] = b.round + 128 }
+          idx = 0
+          for i in ((a-8)..a)
+            @idctpixels[i] = n[idx][0]
+            @idctpixels[i + 640] = n[idx][1]
+            @idctpixels[i + 1280] = n[idx][2]
+            @idctpixels[i + 1920] = n[idx][3]
+            @idctpixels[i + 2560] = n[idx][4]
+            @idctpixels[i + 3200] = n[idx][5]
+            @idctpixels[i + 3840] = n[idx][6]
+            @idctpixels[i + 4480] = n[idx][7]
+            idx += idx
+          end
+          binding.pry
+          box = Matrix[]
+        end
+        if counter < 7
+          counter += 1
+        else
+          counter = 0
+        end
+      end
+    end
+  end
+  def write_encoded_pgm
+    @dctpixels[0]= "P2"
+    @dctpixels[1] = "640 640"
+    @dctpixels[2] = "256"
+    @dctpixels[3] = 0
+    @dctpixels[4] = 0
+    @dctpixels[5] = 0
+    @dctpixels[6] = 0
+    @dctpixels[7] = 0
+    @dctpixels[8] = 0
+    @dctpixels[9] = 0
+    @dctpixels[10] = 0
+    @dctpixels[11] = 0
     IO.write("steph2.pgm", @dctpixels.join("\n"))
   end
 end
@@ -127,7 +192,7 @@ describe MyJPEG, '.sabe o tamanho do arquivo e ' do
     expect(imagem.file_size.to_i).to eq(640)
   end
   it 'Separa em chunks de 8x8' do
-    imagem.chunks
-    imagem.write_pgm
+    imagem.encode
+    imagem.decode
   end
 end
